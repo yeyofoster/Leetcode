8504ded2b2ed6f8d65f88fc581a75a0dd34c4d68
EDDER ROBERTO CID VAZQUEZ <eddrcid@ibm.com>
2020-08-21 17:07:18 -0500

    1 Exercise

---
+++ Exercises/Easy/ReverseInteger.java
@@ -0,0 +1,12 @@
+package Exercises.Easy;
+
+public class ReverseInteger {
+    public static int reverse(int x) {
+        if (x < 0) {
+            x = x * -1;
+        } else {
+
+        }
+        return x;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import Exercises.Easy.ReverseInteger;
+
 public class Main {
     public static void main(String[] args) {
-        int[] input = { 8, 3, 5, 2, 1 };
-        // int val = 1234554321;
-        Arrays101.GreatestElementOnRightSide.replaceElements(input);
+        /* int[] input = { 8, 3, 5, 2, 1 }; */
+        int val = 1527;
+        System.out.println(ReverseInteger.reverse(val));
     }
 }
