a44315029b42eb2fc4b84a022e23dc5895a5947b
Edder Cid <yeyo.foster@gmail.com>
2020-09-30 16:46:55 -0500

    ZigzagConversion completed

--- Exercises/Medium/ZigzagConversion.java
+++ Exercises/Medium/ZigzagConversion.java
@@ -4,16 +4,21 @@ public class ZigzagConversion {
     public static String convert(String s, int numRows) {
         String[] subStrings = new String[numRows];
         String output = "";
+        if (s.length() <= numRows || numRows == 1) {
+            return s;
+        }
         for (int i = 0; i < numRows; i++) {
-            if (i + 1 != numRows) {
-                subStrings[i] = String.valueOf(s.charAt(i));
-            } else
-                subStrings[i] = "";
-            for (int j = (numRows - 1) * 2 - i; j < s.length(); j = (j + 2 * i) * subStrings[i].length()) {
-                subStrings[i] += String.valueOf(s.charAt(j));
-
-                if (j + 2 * i < s.length()) {
-                    subStrings[i] += String.valueOf(s.charAt(j + 2 * i));
+            subStrings[i] = String.valueOf(s.charAt(i));
+            for (int j = (numRows - 1) * 2 - i; j < s.length(); j = j + (numRows - 1) * 2) {
+                if (i == 0 || i == numRows - 1) {
+                    if (j + 2 * i < s.length()) {
+                        subStrings[i] += String.valueOf(s.charAt(j + 2 * i));
+                    }
+                } else {
+                    subStrings[i] += String.valueOf(s.charAt(j));
+                    if (j + 2 * i < s.length()) {
+                        subStrings[i] += String.valueOf(s.charAt(j + 2 * i));
+                    }
                 }
             }
             System.out.println(subStrings[i]);
