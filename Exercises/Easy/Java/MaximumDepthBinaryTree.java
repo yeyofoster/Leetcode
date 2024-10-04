1f5c65707f5b7ebe1246c59a31ac41ec024d6203
Edder Cid <yeyo.foster@gmail.com>
2024-10-04 03:15:02 -0600

    Import TreeNode structure in MaximumDepthBinary

--- Exercises/Easy/Java/MaximumDepthBinaryTree.java
+++ Exercises/Easy/Java/MaximumDepthBinaryTree.java
@@ -4,6 +4,8 @@
  */
 package Exercises.Easy.Java;
 
+import Exercises.DataStructures.TreeNode;
+
 public class MaximumDepthBinaryTree {
     public static void main(String[] args) {
         // Input: root = [3,9,20,null,null,15,7]
@@ -24,25 +26,5 @@ public class MaximumDepthBinaryTree {
         }
 
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    }
-
-    // Definition for a binary tree node.
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
     }
 }
